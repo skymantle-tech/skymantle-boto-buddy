acbb430709d06fd0cd6731e9ad4c155205498b64
Artin Yong-Bannayan <ayongbannayan@skymantle.com>
2023-12-30 14:10:15 -0500

    Update ssm so that get_boto3_client is imported directly. Remove empty string from get default region

--- src/skymantle_boto_buddy/ssm.py
+++ src/skymantle_boto_buddy/ssm.py
@@ -1,17 +1,16 @@
 import os
 
-import skymantle_boto_buddy
-from skymantle_boto_buddy import EnableCache
+from skymantle_boto_buddy import EnableCache, get_boto3_client
 
 
 def get_ssm_client(region_name: str, enable_cache: EnableCache = EnableCache.YES):
     if enable_cache == EnableCache.YES:
-        return skymantle_boto_buddy.get_boto3_client("ssm", region_name)
+        return get_boto3_client("ssm", region_name)
     else:
-        return skymantle_boto_buddy.get_boto3_client.__wrapped__("ssm", region_name)
+        return get_boto3_client.__wrapped__("ssm", region_name)
 
 
-default_region: str = os.environ.get("AWS_DEFAULT_REGION", "")
+default_region: str = os.environ.get("AWS_DEFAULT_REGION")
 if default_region:
     get_ssm_client(default_region)
 
@@ -19,6 +18,7 @@ if default_region:
 def get_parameter(key: str, region_name: str = default_region) -> str:
     client = get_ssm_client(region_name)
     response = client.get_parameter(Name=key)
+
     value = response.get("Parameter", {}).get("Value")
 
     return value
